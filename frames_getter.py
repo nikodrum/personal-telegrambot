37a9209c3757ee33c93dfd1814bf444c9055384c
Nikolai <prosto2307@gmail.com>
2017-04-09 14:51:22 +0300

    added script for downloading, saving, creating gif

---
+++ frames_getter.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+import requests
+import cv2
+import logging
+import time
+
+logging.basicConfig(format=u'%(level_name)-8s [%(asc_time)s] %(message)s',
+                    filename=u'frames_getter.log')
+logger = logging.getLogger(__name__)
+
+
+def set_constants():
+    global INIT_HOURS
+    INIT_HOURS = [h - 3 for h in range(8, 19)]
+    global current_day
+    current_day = datetime.now().date
+    global GIF_NEEDED
+    GIF_NEEDED = True
+
+
+def download_file(url):
+    r = requests.get(url, stream=True)
+    with open("video.mp4", 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    return True
+
+
+def get_frame():
+    cap = cv2.VideoCapture('./data/temp/video.mp4')
+    count = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if count == 10:
+            cv2.imwrite("./data/frames/%d.jpg" % round(time.time(), 0), frame)
+            break
+        count += 1
+    cap.release()
+
+
+def load_all_images(frames_dir):
+    frames = []
+    for file_name in os.listdir(frames_dir):
+        img = cv2.imread(os.path.join(frames_dir, file_name))
+        frames.append(img)
+
+    return frames
+
+
+def build_gif(frames, title=''):
+    figure = plt.figure()
+    ax = figure.add_subplot(111)
+    ax.set_axis_off()
+    ims = map(lambda x: (ax.imshow(x), ax.set_title(title)), frames)
+
+    im_ani = animation.ArtistAnimation(figure, ims, interval=800, repeat_delay=0, blit=False)
+    im_ani.save('./gif/%s.gif' % round(time.time(), 0), writer='imagemagick')
+
+    return True
+
+
+if __name__ == '__main__':
+
+    set_constants()
+
+    while True:
+
+        if datetime.now().hour in INIT_HOURS:
+            INIT_HOURS.remove(datetime.now().hour)
+            download_file("http://vs8.videoprobki.com.ua/tvukrbud/cam17.mp4")
+            get_frame()
+
+        if GIF_NEEDED and datetime.now().hour > 16:
+            frames_list = load_all_images("./data/frames")
+            build_gif(frames=frames_list)
+
+            GIF_NEEDED = False
+
+        if current_day != datetime.now().date:
+            set_constants()
+
+        time.sleep(60 * 60)
