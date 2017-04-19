490758b6fa51ff6c257bb0ff6f9fad31208a6c13
Nikolai <prosto2307@gmail.com>
2017-04-19 21:44:35 +0300

    folder structure, gif making process fixes, logging

--- frames_getter.py
+++ frames_getter.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import os
@@ -15,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 def set_constants():
     global INIT_HOURS
-    INIT_HOURS = [h - 3 for h in range(8, 19)]
+    INIT_HOURS = [h - 3 for h in range(9, 19)]
     global CURRENT_DAY
     CURRENT_DAY = datetime.now().date()
     global GIF_NEEDED
@@ -24,7 +26,7 @@ def set_constants():
 
 def download_file(url):
     r = requests.get(url, stream=True)
-    with open("video.mp4", 'wb') as f:
+    with open("./data/temp/video.mp4", 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
@@ -37,7 +39,7 @@ def get_frame(file):
     while cap.isOpened():
         ret, frame = cap.read()
         if count == 10:
-            cv2.imwrite("./data/frames/%d" % file, frame)
+            cv2.imwrite('./data/frames/{}/{}'.format(datetime.today().date, file), frame)
             break
         count += 1
     cap.release()
@@ -46,22 +48,24 @@ def get_frame(file):
 def load_all_images(frames_dir):
     frames = []
     for frame_name in os.listdir(frames_dir):
-        img = cv2.imread(os.path.join(frames_dir, frame_name))
-        frames.append(img)
+        img = cv2.imread(os.path.join(frames_dir, frame_name), cv2.IMREAD_COLOR)
+        if img is not None:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            frames.append(img)
 
     return frames
 
 
 def build_gif(frames, title=''):
-    figure = plt.figure()
-    ax = figure.add_subplot(111)
+    fig = plt.figure(figsize=[12.8, 7.2], frameon=False)
+    ax = fig.add_axes([0, 0, 1, 1])
     ax.set_axis_off()
-    ims = map(lambda x: (ax.imshow(x), ax.set_title(title)), frames)
+    ims = [(plt.imshow(x), ax.set_title(title)) for x in frames]
+    im_ani = animation.ArtistAnimation(fig, ims, interval=100, repeat_delay=0, blit=True)
 
-    im_ani = animation.ArtistAnimation(figure, ims, interval=800, repeat_delay=0, blit=False)
-    im_ani.save('./gif/%s.gif' % round(time.time(), 0), writer='imagemagick')
+    im_ani.save('./data/gif/%s.gif' % round(time.time(), 0), writer='imagemagick', dpi=60)
 
-    return True
+    return plt.show()
 
 
 if __name__ == '__main__':
@@ -80,7 +84,7 @@ if __name__ == '__main__':
             file_name = str(round(time.time(), 0)) + '.jpg'
             get_frame(file_name)
 
-            if file_name in os.listdir('./data/frames'):
+            if file_name in os.listdir('./data/frames/%s' % datetime.today().date):
                 logger.info("Getting succeed.")
             else:
                 logger.info("Getting failed.")
@@ -89,16 +93,18 @@ if __name__ == '__main__':
 
             logger.info("Started making gif.")
 
-            frames_list = load_all_images("./data/frames")
+            frames_list = load_all_images("./data/frames/%s" % datetime.today().date)
+            logger.info("Got %d frames. " % len(frames_list))
             build_gif(frames=frames_list)
 
             GIF_NEEDED = False
 
-            logger.info("Making succed.")
+            logger.info("Making succeed.")
 
         if CURRENT_DAY != datetime.now().date():
 
             logger.info("Updating constants.")
             set_constants()
+            os.makedirs('./data/frames/%s' % datetime.today().date)
 
         time.sleep(60 * 60)
