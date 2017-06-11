6c194b9a95d55ce65f476d7b063678ad74082e8e
Nikolai <prosto2307@gmail.com>
2017-06-11 19:55:17 +0300

    host is now getting from environment vars

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ from logger import logger
 import requests
 import time
 
-WEBHOOK_HOST = 'provide here server ip'
+WEBHOOK_HOST = os.environ['HOST']
 WEBHOOK_PORT = 80
-WEBHOOK_LISTEN = 'provide here server ip'
+WEBHOOK_LISTEN = os.environ['HOST']
 
 WEBHOOK_SSL_CERT = './webhook/webhook_cert.pem'
 WEBHOOK_SSL_PRIV = './webhook/webhook_pkey.pem'
