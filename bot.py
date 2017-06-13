3ce514b093cd0f0c23c1360592ce71689e02112e
Nikolai <prosto2307@gmail.com>
2017-06-13 23:33:34 +0300

    standardized the way how frames is getting anf gifs is building

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import cherrypy
 import telebot
 import time
-from models import Frame
+from datetime import datetime
+from models import Frame, Recognize, Gif
 from config import *
 
 bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
@@ -27,15 +28,31 @@ class BotServer(object):
     @staticmethod
     @bot.message_handler(content_types=["text"])
     def repeat_all_messages(message):
-        start_time = 0
-        cherrypy.log("Processing request from {}".format(message.chat.id))
-        if int(time.time()) - start_time > 15 * 60:
-            frame = Frame()
-            frame.download_video("http://vs8.videoprobki.com.ua/tvukrbud/cam17.mp4")
-            frame.get()
-        cherrypy.log("File size is :" + str(os.stat('frame10.jpg').st_size))
-        bot.send_photo(message.chat.id, open('frame10.jpg', 'rb'))
+        u_id = message.chat.id
+        cherrypy.log("Processing request from {}.".format(u_id))
 
+        request = Recognize(message)
+        cherrypy.log("Preparing {} for {}.".format(request, u_id))
+
+        today_str = str(datetime.now().date())
+        if request is "frame":
+            frame = Frame(file_path="./data/temp/frame.jpg")
+            frame.download_video("http://vs8.videoprobki.com.ua/tvukrbud/cam17.mp4")
+            frame_path = frame.get()
+
+            cherrypy.log("File size is : " % os.stat(frame_path).st_size)
+            bot.send_photo(u_id, open(frame_path, 'rb'))
+
+        if request is "gif":
+            gif_path = "./data/{}/{}.gif".format(request, today_str)
+            if os.path.exists(gif_path):
+                bot.send_photo(u_id, open('./data/gif/%s.gif' % today_str, 'rb'))
+            else:
+                gif = Gif(today_str)
+                if gif.create():
+                    bot.send_photo(u_id, open('./data/gif/%s.gif' % today_str, 'rb'))
+                else:
+                    bot.send_message(u_id, "Не получается сохранить гифку 😭")
 
 cherrypy.config.update({
     'server.socket_host': WEBHOOK_LISTEN,
@@ -43,9 +60,9 @@ cherrypy.config.update({
     'server.ssl_module': 'builtin',
     'server.ssl_certificate': WEBHOOK_SSL_CERT,
     'server.ssl_private_key': WEBHOOK_SSL_PRIV,
-    'log.screen': False,
-    'log.error_file': "./logs/error.log",
-    'log.access_file': "./logs/access_file.log"
+    'logs.screen': False,
+    'logs.error_file': "./logs/error.logs",
+    'logs.access_file': "./logs/access_file.logs"
 })
 
 
--- models.py
+++ models.py
@@ -1,29 +1,108 @@
-import requests
+import cherrypy
 import cv2
+import matplotlib
+import os
+import re
+import requests
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from datetime import datetime
 
 
 class Frame(object):
-
-    def __init__(self, url="http://vs8.videoprobki.com.ua/tvukrbud/cam17.mp4"):
+    def __init__(self, file_path, url="http://vs8.videoprobki.com.ua/tvukrbud/cam17.mp4"):
         self.url = url
+        self.file_path = file_path
 
     @staticmethod
     def download_video(url):
         r = requests.get(url, stream=True)
-        with open("video.mp4", 'wb') as f:
+        with open("./data/temp/video.mp4", 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
         return True
 
-    @staticmethod
-    def get():
-        cap = cv2.VideoCapture('./video.mp4')
+    def get(self):
+
+        cap = cv2.VideoCapture('./data/temp/video.mp4')
         count = 0
         while cap.isOpened():
             ret, frame = cap.read()
             if count == 10:
-                cv2.imwrite("frame%d.jpg" % count, frame)
+                cv2.imwrite(self.file_path, frame)
                 break
             count += 1
         cap.release()
+        return self.file_path
+
+
+class Gif(object):
+
+    def __init__(self, today_day: str):
+        self.today_day = today_day
+        self.filename = "{}.{}".format(today_day, "gif")
+        self.frames_dir = "./data/frames/"
+
+    def load_all_images(self, date: str):
+
+        frames = []
+        frames_date_dir = self.frames_dir + date
+        if not os.path.exists(frames_date_dir):
+            os.makedirs(frames_date_dir)
+
+        frame_names_list = os.listdir(frames_date_dir)
+        cherrypy.log("Found {} frames files at {}.".format(len(frame_names_list),
+                                                           frames_date_dir))
+
+        for frame_name in frame_names_list:
+            img = cv2.imread(os.path.join(frames_date_dir, frame_name), cv2.IMREAD_COLOR)
+            if img is not None:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                frames.append(img)
+
+        return frames
+
+    def build(self, frames, title=''):
+
+        cherrypy.log("Started making new gif...")
+        fig = plt.figure(figsize=[12.8, 7.2], frameon=False)
+        ax = fig.add_axes([0, 0, 1, 1])
+        ax.set_axis_off()
+        ims = [(plt.imshow(x), ax.set_title(title)) for x in frames]
+        im_ani = animation.ArtistAnimation(fig, ims, interval=100, repeat_delay=0, blit=True)
+        cherrypy.log("GIF successfully created.")
+        try:
+            file_path = './data/gif/%s' % self.filename
+            im_ani.save(file_path, writer='imagemagick', dpi=60)
+            cherrypy.log("GIF successfully saved. File size is {}kb.".format(
+                str(round(os.stat(file_path).st_size/1024, 0))))
+        except Exception:
+            cherrypy.log.error("Saving GIF failed with error ", traceback=True)
+            return False
+        plt.close()
+        return True
+
+    def create(self):
+
+        frames = self.load_all_images(self.today_day)
+        if len(frames) != 0:
+            return self.build(frames=frames)
+
+        return False
+
+
+class Recognize(object):
+
+    def __init__(self, message):
+        self.message = message
+
+    def recognize(self):
+        if re.findall(r"гиф", self.message):
+            return "gif"
+        else:
+            return "frame"
+
+    def __call__(self, *args, **kwargs):
+        self.recognize()
