3ce04ba5263f5f45a320aed1fd82e543149fc730
Nikolai <prosto2307@gmail.com>
2017-07-12 19:48:19 +0300

    Revert "refactoring code"
    This reverts commit bbd6c2adbb3e10b4b9bcc4657adf050ec6131c44.

--- frames_getter.py
+++ frames_getter.py
@@ -16,8 +16,14 @@ def get_sunset_sunrise():
     )
     weather_data = json.loads(resp.content.decode())
 
-    sunrise_utc = datetime.fromtimestamp(weather_data['sys']['sunrise'])
-    sunset_utc = datetime.fromtimestamp(weather_data['sys']['sunset'])
+    sunrise_local = datetime.fromtimestamp(weather_data['sys']['sunrise'])\
+        .replace(tzinfo=timezone('Europe/Kiev'))
+    sunrise_utc = sunrise_local.astimezone(timezone('UTC'))
+
+    sunset_local = datetime.fromtimestamp(weather_data['sys']['sunset'])\
+        .replace(tzinfo=timezone('Europe/Kiev'))
+    sunset_utc = sunset_local.astimezone(timezone('UTC'))
+
     return sunrise_utc, sunset_utc
 
 
